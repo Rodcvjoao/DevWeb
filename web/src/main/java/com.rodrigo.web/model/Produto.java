75088887d522f4b8bb1dfdb9eb6e93d12907271d
rodcvjoao <rodcjoao@gmail.com>
2025-08-25 10:24:36 -0300

    Feat: Getters and Setters Produto

--- web/src/main/java/com.rodrigo.web/model/Produto.java
+++ web/src/main/java/com.rodrigo.web/model/Produto.java
@@ -3,7 +3,14 @@ package com.rodrigo.web.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue
     private Long id;
     private String imagem;
     private String nome;
@@ -14,4 +21,88 @@ public class Produto {
     private BigDecimal preco;
     private LocalDate dataCadastro;
     private String categoria;
+
+    // --- Getters ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    // --- Setters ---
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+    
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
